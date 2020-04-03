4605d2456577535c46453bd50ce10157a972ef24
BELAHOUEL SOFIAN <Sofian@localhost.localdomain>
2020-04-03 14:59:31 +0200

    ADDING --> lib/my/my_exp.c {This file contains a function that return an exponential.}

---
+++ lib/my/my_exp.c
@@ -0,0 +1,32 @@
+/*
+** EPITECH PROJECT, 2020
+** my_exp.c
+** File description:
+** This function return the exponetial of a number.
+*/
+
+#include "my.h"
+
+float exp_neg(float nb)
+{
+    float result = 0;
+
+    result = 1 / nb;
+    return (result);
+}
+
+float my_exp(float nb)
+{
+    float result = 1;
+    int negative = 0;
+
+    if (nb < 0.0) {
+        negative = TRUE;
+        nb *= -1;
+    }
+    for (; nb > 0; nb--)
+        result *= EXP;
+    if (negative == TRUE)
+        result = exp_neg(result);
+    return (result);
+}
