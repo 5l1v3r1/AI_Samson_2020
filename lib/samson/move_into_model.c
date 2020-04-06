29129d2defbfffd1d1ff46137b3576a123148ff3
BELAHOUEL SOFIAN <Sofian@localhost.localdomain>
2020-04-06 19:38:17 +0200

    ADDING --> lib/samson/move_into_model.c {This file contains all the functions that allow to navigate into the brain.}

---
+++ lib/samson/move_into_model.c
@@ -0,0 +1,34 @@
+/*
+** BELAHOUEL'S PROJECT, 2020
+** move_into_model.c
+** File description:
+** This file contains all the function that allow to find elem in the model.
+*/
+
+#include "samson.h"
+
+neuron_t *sms_find_neuron_by_id(layer_t *layer, int id)
+{
+    neuron_t *current = NULL;
+
+    if (layer == NULL || layer->first == NULL)
+        return (NULL);
+    current = layer->first;
+    while (current->id != id && current->next != NULL) {
+        current = current->next;
+    }
+    return (current);
+}
+
+layer_t *sms_find_layer_by_id(model_t *model, int id)
+{
+    layer_t *current = NULL;
+
+    if (model == NULL || model->first == NULL)
+        return (NULL);
+    current = model->first;
+    while (current->id != id && current->next != NULL) {
+        current = current->next;
+    }
+    return (current);
+}
