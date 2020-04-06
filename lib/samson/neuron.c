58d6f30be0f56440b8d7bee37088d90e717775d9
BELAHOUEL SOFIAN <Sofian@localhost.localdomain>
2020-04-06 19:37:11 +0200

    ADDING --> ./lib/samson/neuron.c {This file contains all the functions that allow to build neurons}

---
+++ lib/samson/neuron.c
@@ -0,0 +1,75 @@
+/*
+** BELAHOUEL LABORATORY'S PROJECT, 2020
+** neuron.c
+** File description:
+** This file contains all functions that allow to build neurons.
+*/
+
+#include "samson.h"
+
+float *sms_clean_float_array(float *array, int lenght)
+{
+    for (int i = 0; i < lenght; i++)
+        array[i] = 0.1;//not 0 bcs need to be multiplicate w/ input-> x * 0 = 0
+    return (array);
+}
+
+neuron_t *sms_prepare_new_neuron(neuron_t *new, int id)
+{
+    if (new == NULL)
+        return (NULL);
+    new->id = id;
+    new->probability = 0.1;
+    new->next = NULL;
+    return (new);
+}
+
+neuron_t *sms_get_last_neuron(neuron_t *current)
+{
+    while (current != NULL && current->next != NULL)
+        current = current->next;
+    return (current);
+}
+
+int sms_rm_neuron(layer_t *layer)
+{
+    neuron_t *current = NULL;
+    neuron_t *to_rm = NULL;
+
+    if (layer == NULL || layer->first == NULL)
+        return (ERROR);
+    current = layer->first;
+    while (current->next != NULL) {
+        to_rm = current;
+        current = current->next;
+        free(to_rm->weight);
+        free(to_rm);
+    }
+    free(current->weight);
+    free(current);
+    return (SUCCES);
+}
+
+int sms_add_neuron(layer_t *layer, int id)
+{
+    neuron_t *new_neuron = malloc(sizeof(neuron_t));
+    neuron_t *current_neuron = NULL;
+    float *weight = NULL;
+
+    if (layer == NULL || new_neuron == NULL)
+        return (ERROR);
+    new_neuron = sms_prepare_new_neuron(new_neuron, id);
+    if (new_neuron == NULL)
+        return (ERROR);
+    current_neuron = sms_get_last_neuron(layer->first);
+    weight = malloc(sizeof(float) * (layer->nb_neuron + 1));
+    if (weight == NULL)
+        return (ERROR);
+    weight = sms_clean_float_array(weight, layer->nb_neuron + 1);
+    new_neuron->weight = weight;
+    if (current_neuron == NULL)
+        layer->first = new_neuron;
+    else
+        current_neuron->next = new_neuron;
+    return (SUCCES);
+}
