0fd4c74f874cf7804f536dabd0a21ce157f13cf2
BELAHOUEL SOFIAN <Sofian@localhost.localdomain>
2020-04-06 19:34:11 +0200

    ADDING --> lib/samson/int_encoder.c {This file will contains a function that allow to encode data into int.}

---
+++ lib/samson/int_encoder.c
@@ -0,0 +1,15 @@
+/*
+** BELAHOUEL LABORATORY'S PROJECT, 2020
+** int_encoder.c
+** File description:
+** This file contains a function that take a dataset of char * and int and
+** encode the char into int.
+** Params : char **dataset compose of str and int.
+** Return : char **dataset conmpose of int.
+** It return a dataset of type char to limit memory taken.
+*/
+
+char **sms_int_encoder(char **dataset)
+{
+
+}
