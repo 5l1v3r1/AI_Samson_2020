5124645e64a832f31fc1d4354321cd115406bb70
BELAHOUEL SOFIAN <Sofian@localhost.localdomain>
2020-04-08 13:13:15 +0200

    UPDATING --> model.c {Adding feature to get input user NEED TO REFACTOR AND SCALE THIS FILE }

--- lib/samson/model.c
+++ lib/samson/model.c
@@ -7,6 +7,41 @@
 
 #include "samson.h"
 
+float *sms_prepare_dataset(float *dataset, int lenght)
+{
+    dataset = malloc(sizeof(float) * (lenght + 1));
+
+    if (dataset == NULL)
+        return (NULL);
+    for (int i = 0; i < lenght; i++)
+        dataset[i] = 0.0;
+    return (dataset);
+}
+
+int sms_fill_dataset_user(model_t *model, float *user_dtset, int lenght_u)
+{
+    if (model == NULL || model->input_set == NULL || user_dtset == NULL) {
+        write(1, "Model Error : Dataset : Check your input or RAM\n", 48);
+        return (ERROR);
+    } else if (model->nb_input != lenght_u) {
+        write(1, "Model : Error : Expected : ", 27);
+        my_put_nbr(model->nb_input);
+        write(1, " input(s), Got : ", 17);
+        my_put_nbr(lenght_u);
+        write(1, " input(s) \n", 11);
+        return (ERROR);
+    }
+    for (int i = 0; i < model->nb_input; i++) {
+        if (user_dtset[i] < 0.0 || user_dtset[i] > 1.0) {
+            write(1, "Model Error : Dataset should be only digits betwee", 50);
+            write(1, "n 0 and 1\n", 10);
+            return (ERROR);
+        }
+        model->input_set[i] = user_dtset[i];
+    }
+    return (ERROR);
+}
+
 int sms_prepare_model(model_t *model, int nb_input, int nb_output)
 {
     if (model == NULL)
@@ -17,6 +52,12 @@ int sms_prepare_model(model_t *model, int nb_input, int nb_output)
     model->nb_layers = 0;
     model->nb_input = nb_input;
     model->nb_output = nb_output;
+    model->input_set = NULL;
+    model->output_set = NULL;
+    model->input_set = sms_prepare_dataset(model->input_set, nb_input);
+    model->output_set = sms_prepare_dataset(model->output_set, nb_output);
+    if (model->input_set == NULL || model->output_set == NULL)
+        return (ERROR);
     return (SUCCESS);
 }
 
