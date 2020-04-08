269a2fffa1f16d13029f7aa34500a6037e9ce299
BELAHOUEL SOFIAN <Sofian@localhost.localdomain>
2020-04-08 07:41:15 +0200

    UPDATING --> lib/samson.c {Adding feature to know lenght_prev layer}

--- lib/samson/layer.c
+++ lib/samson/layer.c
@@ -7,9 +7,23 @@
 
 #include "samson.h"
 
+int sms_layer_lenght(layer_t *layer)
+{
+    neuron_t *current = NULL;
+    int i = 1;
+
+    if (layer == NULL || layer->first == NULL)
+        return (0);
+    current = layer->first;
+    for (; current != NULL && current->next != NULL; i++)
+        current = current->next;
+    return (i);
+}
+
 layer_t *sms_prepare_new_layer(layer_t *new_layer, int id)
 {
     new_layer->id = id;
+    new_layer->lenght_prev_layer = 0;
     new_layer->nb_neuron = 0;
     new_layer->first = NULL;
     new_layer->next = NULL;
@@ -20,12 +34,15 @@ int sms_add_layer(model_t *model, int nb_neuron)
 {
     layer_t *new_layer = malloc(sizeof(layer_t));
     layer_t *current_layer = NULL;
+    int lngt_p_l = 0;
 
     if (model == NULL || new_layer == NULL)
         return (ERROR);
+    if (current_layer != NULL)
+        lngt_p_l = sms_layer_lenght(current_layer);
     new_layer = sms_prepare_new_layer(new_layer, model->nb_layers);
     for (; new_layer->nb_neuron < nb_neuron; new_layer->nb_neuron++) {
-        if (sms_add_neuron(new_layer, new_layer->nb_neuron) == ERROR)
+        if (sms_add_neuron(new_layer, new_layer->nb_neuron, lngt_p_l) == ERROR)
             return (ERROR);
     }
     current_layer = model->first;
@@ -33,8 +50,10 @@ int sms_add_layer(model_t *model, int nb_neuron)
         current_layer = current_layer->next;
     if (current_layer == NULL)
         model->first = new_layer;
-    else
+    else {
+        new_layer->lenght_prev_layer = lngt_p_l;
         current_layer->next = new_layer;
+    }
     model->nb_layers += 1;
     return (SUCCESS);
 }
