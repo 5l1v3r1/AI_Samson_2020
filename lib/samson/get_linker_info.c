3292114c7f827b0b4067ddbef68f30f4ba552356
BELAHOUEL SOFIAN <Sofian@localhost.localdomain>
2020-04-10 10:27:34 +0200

    UPDATING --> lib/samson/get_linker_info.c {Rm old function call}

--- lib/samson/get_linker_info.c
+++ lib/samson/get_linker_info.c
@@ -11,10 +11,8 @@
 int proces_link_nrn(layer_t *layer1, neuron_t *nrn_lay2)
 {
     for (int n = 0; n < layer1->nb_neuron; n++) {
-        if (sms_link_two_neuron_together(layer1, nrn_lay2, n) == ERROR) {
-            sms_display_link_two_layer_together(ERROR, 0, 0);
+        if (sms_link_two_neuron_together(layer1, nrn_lay2, n) == ERROR)
             return (ERROR);
-        }
     }
     return (SUCCESS);
 }
